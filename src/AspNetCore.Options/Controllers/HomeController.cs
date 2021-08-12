bd5e8731f1836f011bbb8ca12c405312ffa7bae8
samuelp101 <samuelphung@live.com>
2021-08-11 22:15:20 -0700

    upload sample codes

---
+++ src/AspNetCore.Options/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using AspNetCore.Options.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+
+namespace AspNetCore.Options.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOptions<AccountConfigOptions> _options;
+
+        public HomeController(ILogger<HomeController> logger, IOptions<AccountConfigOptions> options)
+        {
+            _logger = logger;
+            _options = options;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ShowAccountConfig()
+        {
+            return View(_options.Value);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/AspNetCore.Options/Models/AccountConfigOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCore.Options.Models
+{
+    public class AccountConfigOptions
+    {
+        public string Company { get; set; }
+        public string Username { get; set; }
+        public string Region { get; set; }
+        public string SalesRep { get; set; }
+    }
+}
